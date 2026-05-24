a585d32fb11509d860034b4e77f6a709bd5dd8b5
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-05-24 16:58:24 +0300

    fix: Active button class assignment.

--- src/components/menu/menu-bar.tsx
+++ src/components/menu/menu-bar.tsx
@@ -7,13 +7,13 @@ export default function MenuBar({ selectedBtn, setSelectedBtn }: MenuBarProps) {
   return (
     <div className="button-row">
       <button
-        className={`btn ${selectedBtn ? "active-btn" : ""}`}
+        className={`btn ${selectedBtn === "all-data" ? "active-btn" : ""}`}
         onClick={() => setSelectedBtn("all-data")}
       >
         All data
       </button>
       <button
-        className={`btn ${selectedBtn ? "active-btn" : ""}`}
+        className={`btn ${selectedBtn === "total-amount" ? "active-btn" : ""}`}
         onClick={() => setSelectedBtn("total-amount")}
       >
         Total Amount
