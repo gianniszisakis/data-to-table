1f9c22345a23a03eadbf44fbf4767b8c952585a2
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-05-24 18:04:32 +0300

    feat: Show only paid orders.

--- src/components/tables/all-data-table.tsx
+++ src/components/tables/all-data-table.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Order } from "../../models/types";
 
 interface AllDataTableProps {
@@ -5,9 +6,60 @@ interface AllDataTableProps {
 }
 
 export default function AllDataTable({ data }: AllDataTableProps) {
+  const [showPaid, setShowPaid] = useState(false);
+
+  const paid_orders = data?.filter((order: Order) => order?.status === "paid");
+
+  function tableRender() {
+    switch (showPaid) {
+      case true:
+        return paid_orders && paid_orders.length > 0 ? (
+          paid_orders.map((order: Order) => (
+            <tr key={order.id}>
+              <td>{order.id ?? "-"}</td>
+              <td>{order.customer?.name ?? "-"}</td>
+              <td>{order.customer?.city ?? "-"}</td>
+              <td>{order.status ?? "-"}</td>
+              <td>{order.createdAt ?? "-"}</td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={5}>No Paid Orders</td>
+          </tr>
+        );
+      case false:
+        return data?.map((order: Order) => (
+          <tr key={order?.id}>
+            <td>{order?.id ?? "-"}</td>
+            <td>{order?.customer?.name ?? "-"}</td>
+            <td>{order?.customer?.city ?? "-"}</td>
+            <td>{order?.status ?? "-"}</td>
+            <td>{order?.createdAt ?? "-"}</td>
+          </tr>
+        ));
+        break;
+      default:
+        return data?.map((order: Order) => (
+          <tr key={order?.id}>
+            <td>{order?.id ?? "-"}</td>
+            <td>{order?.customer?.name ?? "-"}</td>
+            <td>{order?.customer?.city ?? "-"}</td>
+            <td>{order?.status ?? "-"}</td>
+            <td>{order?.createdAt ?? "-"}</td>
+          </tr>
+        ));
+    }
+  }
+
   return (
     <>
-      <h1>All data table</h1>
+      <h1>{`${showPaid ? `Paid Orders (${paid_orders?.length})` : `All data table (${data?.length})`}`}</h1>
+      <div className="w-[50%]">
+        <button className="btn" onClick={() => setShowPaid(!showPaid)}>
+          {showPaid ? "Paid" : "All data"}
+        </button>
+      </div>
       <div className="table-container">
         {data?.length && data?.length > 0 ? (
           <table className="custom-table">
@@ -21,20 +73,12 @@ export default function AllDataTable({ data }: AllDataTableProps) {
               </tr>
             </thead>
 
-            <tbody>
-              {data?.map((order: Order) => (
-                <tr key={order?.id}>
-                  <td>{order?.id ?? "-"}</td>
-                  <td>{order?.customer?.name ?? "-"}</td>
-                  <td>{order?.customer?.city ?? "-"}</td>
-                  <td>{order?.status ?? "-"}</td>
-                  <td>{order?.createdAt ?? "-"}</td>
-                </tr>
-              ))}
-            </tbody>
+            <tbody>{tableRender()}</tbody>
           </table>
         ) : (
-          <p>No data exist</p>
+          <tr>
+            <td colSpan={5}>No Data</td>
+          </tr>
         )}
       </div>
     </>
